2644e744eafd8434bf61e07158b4290c9996affb
Trinidad Moruga Scorpion <phixpc@protonmail.com>
2026-01-07 03:50:14 +0200

    Check the image also.
    Now trying to add more code than data by allowing the rules github has set to avoid exess flood. And binary output graph.
    
    http://searchengine.dy.fi/

---
+++ speedtest.php
@@ -0,0 +1,63 @@
+<?php
+//Buld for easy usage for dummies and why not to professionals also??
+//Original research by TMS( http://anothersearchengine.dy.fi/ )
+//gpg key and email address available on my homepage.
+
+
+//To release: anti-ddos protection with 5 different blocking systems for rain. Maybe make this available for with installation help from me to scoped sites by one by one to avoid 
+//Superuser to get bypass zero day like source code against my work for copying too much code. So if you are interested and have working website and want good anti-ddos protection-
+//and know how to use monero or bitcoin, then please email me as topic like a: Digital Fortress - Anti-DDos Protection announce. Email is admin{ at }anothersearchengine.dy.fi
+//we can discuss the price it depends how strong anti-ddos protection is your like. For maxium protection we use 5 different layers that works as "blocks". 不和
+
+//Just ask bravely the anti-ddos protection to your site or any guestions are allowed and even wanted to get to my inbox. If you got any, "any" questions or ideas, etc
+//just email me. I do the radio silence somehow, so if i dont respond, i will be back soon. I have wife and life on my own so computers are my hobby and work to do.
+
+//Sure if you need anti-ddos-protection from me, i will check and guarantee that it is working correctly and primitive without any errors and for your accept, we can-
+//even test the benchmark, and you will see that the server does not give maxium power on bandwidth and has rules behind Digital Fortress.
+
+//05.01.2026 - 21:52:30 TMS Somewhere in earth solarsystem.
+
+//Heres the source code, just for autoinstall wrote by outmemory and tested not yet for barely fresh installed linux that has not any goodies, but will do that and fix the code if any..
+
+//imports sanat .py tiedoston alusta, kun ladataan tarvittavat libraryt..
+
+
+
+
+//python3 requirements start point #######
+
+
+system("sudo apt install python3-csv*");
+system("sudo apt install python3-datetime*");
+system("sudo apt install python3-errno*");
+system("sudo apt install python3-math*");
+system("sudo apt install python3-os*");
+system("sudo apt install python3-re*");
+system("sudo apt install python3-sys*");
+system("sudo apt install python3-xml.parsers.expat*");
+system("sudo apt install python3-platform*");
+system("sudo apt install python3-signal*");
+system("sudo apt install python3-socket*");
+system("sudo apt install python3-threading*");
+system("sudo apt install python3-timeit*");
+system("sudo apt install python3-urllib*");
+system("sudo apt install python3-httplib*");
+system("sudo apt install python3-urlparse*");
+system("sudo apt install python3-argparse*");
+system("sudo apt install python3-md5*");
+//system("sudo apt install gzip*");
+system("sudo apt install python3-gzip*");
+system("sudo apt install python3");
+system("sudo apt install tee");
+
+//end of python3 requirements ¤¤#########
+
+
+
+system("sudo apt install python3");
+system("sudo apt install netstat");
+system("python3 speedtest.py | tee speedtest_results.html");
+
+
+
+?>
